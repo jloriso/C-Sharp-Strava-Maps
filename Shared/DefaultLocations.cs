d2c00f2d0002b5f7d06073077cbb8d9e68d94aed
jloriso <jgloriso@gmail.com>
2026-08-10 17:09:38 -0500

    locations

--- Shared/DefaultLocations.cs
+++ Shared/DefaultLocations.cs
@@ -6,9 +6,9 @@ public static class DefaultLocations
 {
     public static List<MapLocation> Standard() => new()
     {
-        new MapLocation { Name = "USA", Lat = 38.0, Lon = -94.8, Zoom = 5 },
         new MapLocation { Name = "Chicago", Lat = 42.0707, Lon = -87.7368, Zoom = 10 },
         new MapLocation { Name = "Kalamazoo", Lat = 42.2, Lon = -85.6, Zoom = 11 },
+        new MapLocation { Name = "USA", Lat = 38.0, Lon = -94.8, Zoom = 5 },
         new MapLocation { Name = "World", Lat = 20.0, Lon = 0.0, Zoom = 3 },
     };
 }
